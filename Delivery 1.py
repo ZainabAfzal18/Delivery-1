570442867ddbde148438b5a36f041313e24815a2
ZainabAfzal18 <102293078+ZainabAfzal18@users.noreply.github.com>
2022-03-24 16:43:52 +0100

    gtfrd

--- Delivery 1.py
+++ Delivery 1.py
@@ -4,3 +4,13 @@ from dash import html
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pdpip
+
+### DATA IMPORT ###
+
+Excel_file = 'Desktop/Delivery1/Data/my_shop_data.xlsx'
+EmployeesSale = pd.read_excel(Excel_file, "EmployeesSale")
+CategorySale = pd.read_excel(Excel_file, "CategorySale")
+Top5Products = pd.read_excel(Excel_file, "Top5Products")
+Top5Customers = pd.read_excel(Excel_file, "Top5Customers")
+
+
